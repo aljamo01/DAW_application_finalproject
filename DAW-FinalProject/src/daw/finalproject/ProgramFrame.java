f91e2eb9e16ad553e4896c35b8bb9e7fddf9c28f
Mohammed Aljardat <aljamo01@luther.edu>
2018-12-11 07:21:13 +0000

    reverse fix

--- DAW-FinalProject/src/daw/finalproject/ProgramFrame.java
+++ DAW-FinalProject/src/daw/finalproject/ProgramFrame.java
@@ -46,6 +46,24 @@ class ProgramFrame extends JFrame
 
 	    if (currentAction == nextTrackButton)
 		changePlayback(trackNum);
+		if (currentAction == reverseButton) {
++		try {
++		    AudioInputStream audioInputStream =
++			WavEditor.reverse(
++			    AudioSystem.getAudioInputStream(
++				playback.getFile()
++				));
++		    AudioSystem.write(
++			audioInputStream,
++			AudioFileFormat.Type.WAVE,
++			tempSave);
++		    playback.changeFile(tempSave);
++		}
++		// do not care about exceptions.
++		catch(Exception e) {}
++		if (trackNum == currentTrack)
++		    changePlayback(trackNum);
++	    }
 		if (currentAction == resampleButton)
         sampleRateChooser.show((Component) resampleButton, 0, 0);
     }
